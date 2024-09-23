f688d00f54eea7d87575f51d1e9e4cc55143452c
alst161 <alst161@gmail.com>
2024-09-23 03:48:19 +0300

    ssl auto gen

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"github.com/botlabpro/twv/common"
 	"github.com/botlabpro/twv/node"
 	"github.com/botlabpro/twv/stats"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"math/big"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -25,7 +34,39 @@ func main() {
 		context.JSON(200, allNodes)
 	})
 
-	if err := router.RunTLS(":8080", "ssl.pem", "ssl.key"); err != nil {
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1658),
+		Subject: pkix.Name{
+			Organization:  []string{"ORGANIZATION_NAME"},
+			Country:       []string{"COUNTRY_CODE"},
+			Province:      []string{"PROVINCE"},
+			Locality:      []string{"CITY"},
+			StreetAddress: []string{"ADDRESS"},
+			PostalCode:    []string{"POSTAL_CODE"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+
+	// Sign the certificate
+	certificate, _ := x509.CreateCertificate(rand.Reader, cert, cert, pub, priv)
+
+	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certificate})
+	keyBytes := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+
+	// Generate a key pair from your pem-encoded cert and key ([]byte).
+	x509Cert, _ := tls.X509KeyPair(certBytes, keyBytes)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{x509Cert}}
+	server := http.Server{Addr: ":3000", Handler: router, TLSConfig: tlsConfig}
+
+	if err := server.ListenAndServeTLS("", ""); err != nil {
 		panic(err)
 	}
 	/*for {
